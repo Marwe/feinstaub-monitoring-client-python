de02200f14524372e767bf6495ba418a82d42c64
Martin Weis <mwgit.20.mweis@spamgourmet.com>
2018-06-27 10:22:42 +0200

    all missing zapp

--- ffrouterclients.sh
+++ ffrouterclients.sh
@@ -2,7 +2,7 @@
 
 # connect to your freifunk (ff) router, get clients 
 # MAC Prefix: 18:fe:34=Espressif Inc.
-macp='18:fe:34[:0-9a-f]*\|5c:cf:7f[:0-9a-f]*'
+macp='\b([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}\b'
 iwdev="client0"
 # to find out the iw device:
 # iwdev=$(ssh root@172.21.24.254 iwinfo | grep Freifunk| awk '{print $1}')
@@ -13,23 +13,21 @@ if [ -f "$pubkey" ] ; then
 fi
 
 # ff next node IP: 172.21.24.254
-# ff next note IP6: fd21:b4dc:4b1e::1
+# ff next node IP6: fd21:b4dc:4b1e::1
 # you need to have key-based ssh access to your ff router 
 # https://wiki.freifunk-stuttgart.net/anleitungen:config_mode:start#fernzugriff
 # ip="fd21:b4dc:4b1e:0:ea94:f6ff:fe68:ebfe"
 
 if [ -z "$1" ] ; then
-	ip="172.21.24.254"
-	ip="fd21:b4dc:4b1e::1"
+	#ip="172.21.24.254"
+	#ip="fd21:b4dc:4b1e::1"
+	# new nextnodeip
+	ip=fd21:711::1
 else
 	ip="$1"
 fi
 
-if [ "$1" == "marwe" ] ; then
-	ip="fd21:b4dc:4b1e:0:ea94:f6ff:fe68:ebfe"
-fi
-
-ssh $sshopts root@$ip iw dev $iwdev station dump | grep -A 17 -i "$macp" | grep -i "$macp\|inactive\|signal\|bitrate"
+ssh $sshopts root@$ip iw dev $iwdev station dump | grep -E "$macp\|inactive\|signal\|bitrate"
 
 # find chipid.csv
 
@@ -42,10 +40,10 @@ fi
 
 if [ -f "$chipidfile" ] ; then
 	for mac in $(ssh $sshopts "root@$ip" iw dev $iwdev station dump | \
-		grep -i $macp | \
-		grep -o -i "$macp")
+		grep -i -E "$macp" | \
+		grep -o -i -E "$macp")
 	do
- 		grep -i $mac $chipidfile
+ 		grep -i "$mac" $chipidfile
 	done
 fi
 
